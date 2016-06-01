577ceed6bac13e0a1ab7ef5877f284c65245fd6c
jinxu <541769442@qq.com>
2016-06-01 10:35:36 +0800

    2016.6.1 commit

--- app/src/main/java/com/xmz/handson10/HomeActivity.java
+++ app/src/main/java/com/xmz/handson10/HomeActivity.java
@@ -14,7 +14,6 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
